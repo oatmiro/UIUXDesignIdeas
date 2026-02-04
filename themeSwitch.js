19c715f993ffdf36b005b51bcd6b58788dd0baee
oatmiro <113068376+oatmiro@users.noreply.github.com>
2026-02-03 18:52:38 -0700

    Changes to colors and font

--- themeSwitch.js
+++ themeSwitch.js
@@ -2,4 +2,9 @@
 function toggleTheme() {
     var element = document.body;
     element.classList.toggle("darkMode");
+
+    document.querySelectorAll(".generateButton").forEach(btn => {
+        btn.classList.toggle("darkMode");
+    });
+
  }
